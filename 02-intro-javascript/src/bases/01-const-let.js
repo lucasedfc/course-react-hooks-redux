aa8b11abb8b371ec53b08d44a46bcbb9c1d9bba6
Lucas Heim <lucasedfc@gmail.com>
2020-07-04 11:22:07 -0300

    template strings

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,19 @@
+// let and const
+
+const name = 'Luke';
+const lastName = 'Skywalker';
+
+let squareValue = 5;
+
+squareValue = 4;
+
+console.log(name, lastName, squareValue);
+
+if (true) {
+    const name = 'Peter';
+
+    console.log(name);
+
+}
+
+console.log(squareValue);
