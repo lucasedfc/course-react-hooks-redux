313799c49ccb9fda23e8053413f0c979efd6214c
Lucas Heim <lucasedfc@gmail.com>
2020-07-05 17:00:23 -0300

    Init Counter app

---
+++ 03-counter-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FirstApp from './FirstApp';
+import './index.css';
+
+
+const divRoot = document.querySelector('#root');
+
+ReactDOM.render( <FirstApp /> , divRoot )
+
+
