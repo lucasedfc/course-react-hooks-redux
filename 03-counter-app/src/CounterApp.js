ddd36bda6b96eac0223fac08afed04b7cdd93d69
Lucas Heim <lucasedfc@gmail.com>
2020-07-05 18:00:20 -0300

    Click Event

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -3,10 +3,18 @@ import PropTypes from 'prop-types';
 
 const CounterApp = ({ value }) => {
 
+    // handle add
+    const handleAdd = (e) => {
+        console.log(e);
+        
+    }
+
     return (
         <>
             <h1>Counter App</h1>
             <p>{value}</p>
+
+            <button onClick= { handleAdd }>+1</button>
         </>
     );
 }
