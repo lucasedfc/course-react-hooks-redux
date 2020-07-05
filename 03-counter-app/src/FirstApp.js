4d035a074f21c0540e51b1e5dc2decc846d49873
Lucas Heim <lucasedfc@gmail.com>
2020-07-05 17:08:34 -0300

    Fragment

--- 03-counter-app/src/FirstApp.js
+++ 03-counter-app/src/FirstApp.js
@@ -1,8 +1,16 @@
+//import React, { Fragment } from 'react';
 import React from 'react';
 
 const FirstApp = () => {
 
-    return <h1>Hello world!</h1>;
+    return (
+        <>
+            <h1>Hello world!</h1>
+            <p>Counter App</p>
+        </>
+        
+    );
+
 
 }
 
