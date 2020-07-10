fd26a6a6652ab26a591ec630292eaa998db282dd
Lucas Heim <lucasedfc@gmail.com>
2020-07-10 18:12:54 -0300

    Counter App - Reset button

--- 03-counter-app/src/tests/CounterApp.test.js
+++ 03-counter-app/src/tests/CounterApp.test.js
@@ -4,7 +4,7 @@ import React from 'react';
 import CounterApp from '../CounterApp';
 
 describe('Testing CounterApp', () => {
-    
+
     let wrapper = shallow(<CounterApp />);
 
     beforeEach(() => {
@@ -14,13 +14,13 @@ describe('Testing CounterApp', () => {
     test('should return the counter app component correctly', async () => {
 
         expect(wrapper).toMatchSnapshot();
-        
+
     })
 
     test('should validate the value of the counter', () => {
-        
-        
-        const wrapper = shallow(<CounterApp value={100}/>);
+
+
+        const wrapper = shallow(<CounterApp value={100} />);
 
         const expectedValue = wrapper.find('h2').text().trim();
 
@@ -28,29 +28,35 @@ describe('Testing CounterApp', () => {
     })
 
     test('should increment with the add button', () => {
-        
+
         wrapper.find('button').at(0).simulate('click');
         // const btn2 = wrapper.find('button').at(1);
         // const btn3 = wrapper.find('button').at(2);
         const counterText = wrapper.find('h2').text().trim();
 
         expect(counterText).toBe('11');
-        
+
     })
 
     test('should decrease with the remove button', () => {
 
-        
         wrapper.find('button').at(2).simulate('click');
-        // const btn2 = wrapper.find('button').at(1);
-        // const btn3 = wrapper.find('button').at(2);
         const counterText = wrapper.find('h2').text().trim();
-        
-
         expect(counterText).toBe('9');
-        
+
     })
-    
-    
-    
+
+
+    test('should reset value with the reset button', () => {
+
+        const wrapper = shallow(<CounterApp value={110} />);
+
+        wrapper.find('button').at(0).simulate('click');
+        wrapper.find('button').at(0).simulate('click');
+        wrapper.find('button').at(1).simulate('click');
+
+        const counterText = wrapper.find('h2').text().trim();
+        expect(counterText).toBe('110');
+    })
+
 })
