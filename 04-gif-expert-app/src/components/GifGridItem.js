e3b325fbcea64067ac7229ab30578e7e558a290f
Lucas Heim <lucasedfc@gmail.com>
2020-07-26 20:32:02 -0300

    Animate css

--- 04-gif-expert-app/src/components/GifGridItem.js
+++ 04-gif-expert-app/src/components/GifGridItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const GifGridItem = ( {img} ) => {
 
     return (
-        <div className="card">
+        <div className="card animate__animated animate__flipInX">
             <img src={img.url} alt={img.title} />
             <p>{img.title}</p>
         </div>
